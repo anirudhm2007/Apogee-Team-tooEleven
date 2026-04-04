02553e2bd0d4e5c2d90fb1a05b3d73d57571bc6e
Anirudh Madhavan <imt_2025006@iiitm.ac.in>
2026-04-04 15:18:34 +0530

    Create config.js

---
+++ apogee-reactjs/src/config.js
@@ -0,0 +1,6 @@
+const HOST = window.location.hostname;
+const PORT = 5000;
+
+export const BASE_URL   = `http://${HOST}:${PORT}`;
+export const SOCKET_URL = `http://${HOST}:${PORT}`;
+export default BASE_URL;
